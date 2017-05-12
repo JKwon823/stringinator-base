b5473aab0671538aa31ca0ca00d80f7478ae3a7f
Joseph Kwon <kwonjosepho@gmail.com>
2017-05-12 02:50:57 -0700

    add underbar,stringinator, and tests

--- stringinator.js
+++ stringinator.js
@@ -2,28 +2,42 @@ const _ = require('./underbar');
 
 const first = function(str, n) {
   // Your code goes here
+  if (n === undefined) {
+    return str[0];
+  } else {
+    return str.slice(0, n);
+  }
 };
 
 const last = function(str, n) {
   // Your code goes here
+  if (n === undefined) {
+    return str[str.length - 1];
+  } else {
+    return str.slice(str.length - n, str.length);
+  }
 };
 
 const removeChar = function(str, target) {
   // hint: use _.reject
   // Your code goes here
+  return _.reject(str, value => value === target).join('');
 };
 
 const hasChar = function(str, target) {
   // hint: use _.some
   // Your code goes here
+  return _.some(str, value => value === target);
 };
 
 const isOnlyDigits = function(str) {
   // Your code goes here
+  return _.every(str, value => !isNaN(value));
 };
 
 const filterToOnlyDigits = function(str) {
   // Your code goes here
+  return _.filter(str, value => !isNaN(value) && value !== ' ').join('');
 };
 
 const truncateString = function(val, maxLength) {
@@ -34,14 +48,20 @@ const truncateString = function(val, maxLength) {
 const truncateLongItems = function(obj, maxLength) {
   // hint: use truncateString above
   // Your code goes here
+  return _.map(obj, value => truncateString(value, maxLength));
 };
 
 const countChars = function(str) {
   // Your code goes here
+  const charList = {};
+  _.each(_.uniq(str), value => charList[value] = 0);
+  _.each(str, value => charList[value]++);
+  return charList;
 };
 
 const dedup = function(str) {
   // Your code goes here
+  return _.uniq(str).join('');
 };
 
 module.exports = {
--- test/underbar/every.test.js
+++ test/underbar/every.test.js
@@ -4,15 +4,21 @@ describe('every()', () => {
   describe('processing an array of numbers', () => {
     it('will return true if no callback is supplied', () => {
       // Your code goes here
+      const nums = [1, 3, 5, 7];
+      expect(_.every(nums)).toBe(true);
     });
 
     it('returns true if all numbers in an array are odd and we test for odd numbers', () => {
       // Your code goes here
+      const nums = [1, 3, 5, 7];
+      expect(_.every(nums, num => num % 2 === 1)).toBe(true);
     });
 
     it('returns false if not all numbers in an array are odd and we test for odd numbers', () => {
       // Your code goes here
+      const nums = [1, 3, 5, 6, 7];
+      expect(_.every(nums, num => num % 2 === 1)).toBe(false);
     });
-
+    
   });
 });
